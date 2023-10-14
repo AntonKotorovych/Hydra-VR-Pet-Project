623e2a6e9cb37e622ed3461debc9de8db442f9e0
Anton Kotorovych <akotorovych@gmail.com>
2023-10-14 03:26:03 +0300

    some debugging

--- src/main.js
+++ src/main.js
@@ -1,13 +1,9 @@
 const navHamburgerMenu = document.querySelector('.navbar-hamburger-menu');
 const navOpenButton = document.querySelector('.navbar-hamburger-button--open');
-const navCloseButton = document.querySelector(
-  '.navbar-hamburger-button--close'
-);
+const navCloseButton = document.querySelector('.navbar-hamburger-button--close');
 
 function toggleNavbarButtonsVisibility() {
   const screenWidth = window.innerWidth;
-  console.log(screenWidth);
-
   if (navOpenButton.style.display === 'block') {
     navCloseButton.style.display === 'none';
   } else {
@@ -17,23 +13,22 @@ function toggleNavbarButtonsVisibility() {
   if (screenWidth >= 800) {
     navOpenButton.style.display = 'none';
     navCloseButton.style.display = 'none';
-    navHamburgerMenu.style.display = 'none';
+    navHamburgerMenu.classList.toggle('navbar-hamburger-menu--open');
   } else {
-    if (navCloseButton.style.display === 'none')
-      navOpenButton.style.display = 'block';
+    if (navCloseButton.style.display === 'none') navOpenButton.style.display = 'block';
   }
 }
 
 window.addEventListener('resize', toggleNavbarButtonsVisibility);
 
 navOpenButton.addEventListener('click', () => {
-  navHamburgerMenu.style.display = 'flex';
   navOpenButton.style.display = 'none';
   navCloseButton.style.display = 'block';
+  navHamburgerMenu.classList.toggle('navbar-hamburger-menu--open');
 });
 
 navCloseButton.addEventListener('click', () => {
-  navHamburgerMenu.style.display = 'none';
   navOpenButton.style.display = 'block';
   navCloseButton.style.display = 'none';
+  navHamburgerMenu.classList.toggle('navbar-hamburger-menu--open');
 });
