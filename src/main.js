e4caf51c22f9418940ef2e6dd0317bc0424cca64
Anton Kotorovych <akotorovych@gmail.com>
2023-10-14 04:34:15 +0300

    Implement close nav-menu logic after clicking the link

--- src/main.js
+++ src/main.js
@@ -2,14 +2,16 @@ const navHamburgerMenu = document.querySelector('.navbar-hamburger-menu');
 const navOpenButton = document.querySelector('.navbar-hamburger-button--open');
 const navCloseButton = document.querySelector('.navbar-hamburger-button--close');
 
-navOpenButton.addEventListener('click', function () {
+const navHamburgerMenuToggleShow = () => {
+  navHamburgerMenu.classList.toggle('navbar-hamburger-menu--open');
   navOpenButton.classList.toggle('navbar-button--show');
   navCloseButton.classList.toggle('navbar-button--show');
-  navHamburgerMenu.classList.toggle('navbar-hamburger-menu--open');
+};
+
+navOpenButton.addEventListener('click', function () {
+  navHamburgerMenuToggleShow();
 });
 
 navCloseButton.addEventListener('click', () => {
-  navCloseButton.classList.toggle('navbar-button--show');
-  navOpenButton.classList.toggle('navbar-button--show');
-  navHamburgerMenu.classList.toggle('navbar-hamburger-menu--open');
+  navHamburgerMenuToggleShow();
 });
