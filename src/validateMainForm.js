cc7cc9eb3d5ff4c9558a0e497fb2c2a1e4b28abf
Anton Kotorovych <akotorovych@gmail.com>
2023-10-14 23:07:49 +0300

    ..

---
+++ src/validateMainForm.js
@@ -0,0 +1,11 @@
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const email = document.getElementById('email');
+const tel = document.getElementById('tel');
+const subject = document.getElementById('subject');
+
+firstName.addEventListener('input', () => {
+  console.log(firstName);
+});
+
+function validateMainForm() {}
