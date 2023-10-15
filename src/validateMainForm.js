0cf6ebdee90f22b7d02869a6a84c6b41c3fbb60f
Anton Kotorovych <akotorovych@gmail.com>
2023-10-16 02:28:19 +0300

    add full Name validation

--- src/validateMainForm.js
+++ src/validateMainForm.js
@@ -5,10 +5,37 @@ const lastName = document.querySelector('input[name="last-name"]');
 const email = document.querySelector('input[name="email"]');
 const phoneNumber = document.querySelector('input[name="phone-number"]');
 const subject = document.querySelector('input[name="subject"]');
+const submitBtn = document.querySelector('button[name="submit-btn"]');
 
 console.log(mainForm);
-console.log(firstName);
-console.log(lastName);
-console.log(email);
-console.log(phoneNumber);
-console.log(subject);
+
+mainForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  const formData = {
+    firstName: firstName.value,
+    lastName: lastName.value,
+    email: email.value,
+    phoneNumber: phoneNumber.value,
+    subject: subject.value,
+  };
+
+  function validateFullName(firstName, lastName) {
+    let nameIsValid = true;
+    let lastNameIsValid = true;
+
+    if (!firstName.trim().match(/^[a-zA-Z]{1,25}$/)) {
+      nameIsValid = false;
+    }
+
+    if (!lastName.trim().match(/^[a-zA-Z]{1,25}$/)) {
+      lastNameIsValid = false;
+    }
+
+    return [nameIsValid, lastNameIsValid];
+  }
+
+  const [nameIsValid, lastNameIsValid] = validateFullName(formData.firstName, formData.lastName);
+
+  console.log(formData);
+});
