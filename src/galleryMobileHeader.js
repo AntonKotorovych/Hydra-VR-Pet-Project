46811e6a9c9d03a11da662eb7bf21bc150a78884
Anton Kotorovych <akotorovych@gmail.com>
2023-10-18 04:32:47 +0300

    implement rounded counter from 0 to 2

--- src/galleryMobileHeader.js
+++ src/galleryMobileHeader.js
@@ -6,3 +6,16 @@ let call = document.getElementById('call');
 let message = document.getElementById('message');
 
 console.log(geolocation, call, message);
+
+let counter = 0;
+
+btnRight.addEventListener('click', () => {
+  counter = (counter + 1) % 3;
+  console.log(counter);
+});
+
+btnLeft.addEventListener('click', () => {
+  counter = (counter - 1 + 3) % 3;
+
+  console.log(counter);
+});
