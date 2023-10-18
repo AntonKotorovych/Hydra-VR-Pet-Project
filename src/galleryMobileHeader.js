0cbf0d4efedf9f5786a5c54bb46eeffcb39a2c87
Anton Kotorovych <akotorovych@gmail.com>
2023-10-18 04:07:54 +0300

    start. add position absolute and select elements

---
+++ src/galleryMobileHeader.js
@@ -0,0 +1,8 @@
+const btnLeft = document.querySelector('.btn-mobile-header--left');
+const btnRight = document.querySelector('.btn-mobile-header--right');
+
+let geolocation = document.getElementById('geolocation');
+let call = document.getElementById('call');
+let message = document.getElementById('message');
+
+console.log(geolocation, call, message);
