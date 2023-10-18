6393a257bee8b59790dcfbfad34dac406ed11175
Anton Kotorovych <akotorovych@gmail.com>
2023-10-18 05:25:47 +0300

    debug some small bug after resize screen width below 799px

--- src/galleryMobileHeader.js
+++ src/galleryMobileHeader.js
@@ -5,40 +5,41 @@ let geolocation = document.getElementById('geolocation');
 let call = document.getElementById('call');
 let message = document.getElementById('message');
 
+let counter = 0;
+
+function changeInfoView() {
+  if (counter === 0) {
+    geolocation.style.right = '16%';
+    call.style.right = '-116%';
+    message.style.right = '-216%';
+  } else if (counter === 1) {
+    geolocation.style.right = '116%';
+    call.style.right = '16%';
+    message.style.right = '-116%';
+  } else if (counter === 2) {
+    geolocation.style.right = '216%';
+    call.style.right = '116%';
+    message.style.right = '16%';
+  }
+}
+
+btnRight.addEventListener('click', () => {
+  counter = (counter + 1) % 3;
+  console.log(counter);
+  changeInfoView();
+});
+
+btnLeft.addEventListener('click', () => {
+  counter = (counter - 1 + 3) % 3;
+  changeInfoView();
+  console.log(counter);
+});
+
 window.addEventListener('resize', () => {
-  if (window.innerWidth <= 799) {
-    let counter = 0;
-
-    function changeInfoView() {
-      if (counter === 0) {
-        geolocation.style.right = '16%';
-        call.style.right = '-116%';
-        message.style.right = '-216%';
-      } else if (counter === 1) {
-        geolocation.style.right = '116%';
-        call.style.right = '16%';
-        message.style.right = '-116%';
-      } else if (counter === 2) {
-        geolocation.style.right = '216%';
-        call.style.right = '116%';
-        message.style.right = '16%';
-      }
-    }
-
-    btnRight.addEventListener('click', () => {
-      counter = (counter + 1) % 3;
-      console.log(counter);
-      changeInfoView();
-    });
-
-    btnLeft.addEventListener('click', () => {
-      counter = (counter - 1 + 3) % 3;
-      changeInfoView();
-      console.log(counter);
-    });
-  } else {
+  if (window.innerWidth > 799) {
     geolocation.style.removeProperty('right');
     call.style.removeProperty('right');
     message.style.removeProperty('right');
+    counter = 0;
   }
 });
